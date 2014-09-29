038fd931490f179cdc0bd8d9117159e36c2e4ba7
jasonalmaturner <jasonalmaturner@gmail.com>
2014-09-29 17:08:33 -0600

    just starting on Step 4 of README

--- js/app.js
+++ js/app.js
@@ -4,4 +4,20 @@ app.config(function($routeProvider, $httpProvider){
   $httpProvider.interceptors.push('httpRequestInterceptor');
 
   //router here
+  $routeProvider.when('/', {
+  	templateURL: 'js/home/homeTmpl.html',
+  	controller: 'homeCtrl'
+  }).when('/teams/:team', {
+  	templateURL: 'js/teams/teamTmpl.html',
+  	controller: 'teamCtrl',
+  	resolve: {
+  		teamData: function($route, teamService){
+  			return teamService.getTeamData($route.current.params.team);
+  		}
+  	}
+  }).otherwise({
+  	redirectTo: '/'
+  })
+
+
 });
--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,6 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
-
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+	$scope.teamData = teamData;
+	console.log($scope.teamData);
 });
